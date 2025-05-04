eedcf83171c0da68ffa65314fd33a4addb569b37
ern0 <ern0@linkbroker.hu>
2025-05-04 17:57:04 +0200

    working on parser

--- compiler.js
+++ compiler.js
@@ -12,17 +12,17 @@ self.addEventListener("message", function(event) {
 function split_line(str) {
 
     const result = [];
-    var current = "";
-    var inside_quote = null;
-    var skip_rest = false;
-    var last_delim = ""
+    let current = "";
+    let inside_quote = null;
+    let skip_rest = false;
+    let last_delim = ""
 
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
 
         if (skip_rest) continue;
 
-        if (char == '"' || char == "'") {
+        if ((char == '"') || (char == "'")) {
             if (inside_quote == char) {
                 inside_quote = null;
             } else if (inside_quote == null) {
@@ -36,10 +36,12 @@ function split_line(str) {
             continue;
         }
 
-        if (char === ';') {
+        if (char == ';') {
             skip_rest = true;
             continue;
         }
+
+        char = char.replace("\t", " ");
 
         if (char == ':' || char == ',' || char == ' ') {
             if (current.trim()) {
@@ -68,17 +70,17 @@ function split_line(str) {
 function compile(packet) {
 
     app = {}
-    app.ptr = 0;
-    app.mem = {}
+    app.prg_lines = {}
 
     text = packet["source"].split("\n");
     for (lineno in text) {
-        var line = text[lineno];
-        line = line.replace("\t", " ");
-        line = line.trim();
-        line = split_line(line);
+        const line_text = text[lineno];
+        const line_split = split_line(line_text);
+        if (line_split.length == 0) continue;
+        let line_obj = parse_line(lineno, line_split);
 
-        console.log(line);
+        console.log(line_obj);
+        if (line_obj["error"] != null) break;
     }
 
     result = {};
@@ -86,17 +88,53 @@ function compile(packet) {
     return result;
 }
 
+function parse_line(lineno, line_split) {
 
+    let line_obj = {};
+    line_obj["error"] = null;
+    line_obj["lineno"] = lineno;
 
-function compile_append(instr) {
+    parse_line_label(line_obj, line_split);
+    parse_line_instr(line_obj, line_split);
 
-    app.prog[app.line] = instr;
-    app.line += 1;
+    return line_obj;
 }
 
-function test() {
+function parse_line_label(line_obj, line_split) {
 
-    a = "label: db 1,2,4,'a,b,c',99 ; okay"
-    a = split_line(a)
-    console.log(a)
+    if (line_split[0][0] == ":") {
+        line_obj["label"] = line_split[0].split("^")[1];
+    } else {
+        line_obj["label"] = null;
+    }
+}
+
+function parse_line_instr(line_obj, line_split) {
+
+    let isntr_index = 0;
+    if (line_obj["lineno"]) instr_index = 1;
+
+    const instr_orig = line_split[instr_index];
+    const instr_eff = instr_orig.toLowerCase();
+    line_obj["instr"] = instr_eff;
+    line_obj["args"] = line_split.slice(instr_index + 1);
+
+    parse_check_instr_nof_args(line_obj);
+}
+
+function parse_check_instr_nof_args(line_obj) {
+
+    if (line_obj["instr"][0] == ".") {
+        parse_check_pseudo_instr_nof_args(line_obj);
+    } else {
+        parse_check_real_instr_nof_args(line_obj);
+    }
+}
+
+function parse_check_pseudo_instr_nof_args(line_obj) {
+    // TODO
+}
+
+function parse_check_real_instr_nof_args(line_obj) {
+    // TODO
 }
